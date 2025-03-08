be7bad5c2a170b622c8be69f0f219c5cdf4c3db0
Danuja <danujadanuja25@gmail.com>
2025-03-08 19:50:53 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
       <Navbar/>
 
-      <div className="flex items-center py-40">
+      <div className="flex items-center pt-40">
         <div className="max-w-5xl mx-auto px-4 text-center">
           <p className="text-2xl lg:text-5xl tracking-tight text-[#DEE2E6] mb-6">
             Your Modern Gateway to a <br/> <span className="text-green-600 font-semibold">Safe Login System</span>
@@ -19,7 +19,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="py-12 bg-[#212529]">
+      <div className="bg-[#212529] pt-20">
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-2xl font-semibold text-center text-white mb-12">Features</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -45,7 +45,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="py-12">
+      <div className="py-12 pt-20">
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-2xl font-semibold text-center text-white mb-12">So, how this works?</h2>
           <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0 md:space-x-4">
@@ -53,7 +53,7 @@ export default function Home() {
               <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-100 text-indigo-600 mb-4">
                 <span className="font-semibold">1</span>
               </div>
-              <h3 className="font-medium mb-2 text-[#DEE2E6]">Sign Up</h3>
+              <h3 className="font-medium mb-2 text-white">Sign Up</h3>
               <p className="text-gray-300">
                 Create your account with a unique username and strong password.
               </p>
@@ -62,7 +62,7 @@ export default function Home() {
               <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-100 text-indigo-600 mb-4">
                 <span className="font-semibold">2</span>
               </div>
-              <h3 className="font-medium mb- text-[#DEE2E6]">Log In</h3>
+              <h3 className="font-medium mb- text-white">Log In</h3>
               <p className="text-gray-300">
                 Access your account securely with your credentials.
               </p>
@@ -71,7 +71,7 @@ export default function Home() {
               <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-100 text-indigo-600 mb-4">
                 <span className="font-semibold">3</span>
               </div>
-              <h3 className="font-medium mb-2 text-[#DEE2E6]">Personalized Access</h3>
+              <h3 className="font-medium mb-2 text-white">Personalized Access</h3>
               <p className="text-gray-300">
                 Enjoy a personalized welcome experience.
               </p>
@@ -82,7 +82,7 @@ export default function Home() {
 
       <footer className="py-5 border-t border-white/20">
         <div className="max-w-5xl mx-auto px-4 text-center">
-          <p className="text-gray-600">©2025 SecureConnect. All rights reserved.</p>
+          <p className="text-gray-300">©2025 SecureConnect. All rights reserved.</p>
         </div>
       </footer>
     </div>
